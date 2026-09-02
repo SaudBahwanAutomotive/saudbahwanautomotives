ecb947d8698126cb5d7bc20b3aabb429c3492d83
SaudBahwanAutomotive <faithnenooh@gmail.com>
2026-09-02 22:45:26 +0300

    Initialize Supabase client in app.js

---
+++ app.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://xretkpptkpbjxbbuqrey.supabase.co";
+
+const SUPABASE_KEY =
+  "sb_publishable_XrFWvnnoqoUM5sL5UcbQLg_Z7QhtfKT";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
